41264d191eb5583577b89fa39999b2fdfbca6a1d
khanna01 <govmfkdtm@gmail.com>
2024-08-27 22:55:24 +0900

    [Add] Badges

--- course1/starbucks/js/main.js
+++ course1/starbucks/js/main.js
@@ -1,16 +1,43 @@
 const searchEl = document.querySelector('.search')
 const searchInputEl = searchEl.querySelector('input')
 
+// 검색버튼 클릭하면 검색 입력창에 포커스 줌
 searchEl.addEventListener('click', (e) => {
   searchInputEl.focus()
 })
 
+// 검색아이콘 누르면 검색 입력창이 나타남
 searchInputEl.addEventListener('focus', (e) => {
   searchEl.classList.add('focused')
   searchInputEl.setAttribute('placeholder', '통합검색')
 })
 
+// 검색 입력창 블러
 searchInputEl.addEventListener('blur', () => {
   searchEl.classList.remove('focused')
   searchInputEl.setAttribute('placeholder', '')
 })
+
+const badgeEl = document.querySelector('header .badges')
+
+// 스크롤시 배지 나타나게/사라지게
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // badgeEl.style.display = 'none'
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      })
+    } else {
+      // 배지 보이기
+      // badgeEl.style.display = 'block'
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      })
+    }
+  }, 300),
+)
--- course1/starbucks/note.md
+++ course1/starbucks/note.md
@@ -21,3 +21,21 @@ a태그 링크가 준비되어 있지 않다면 임시로 작성하는 방법 2�
 HTML 클래스 속성의 작명법 - **BEM** (Block Element Modifier)
 - 요소__일부분 : Underscore(Lodash) 기호로 요소의 일부분 표시
 - 요소--상태 : Hyphen(Dash) 기호로 요소의 상태 표시
+
+<hr> 
+
+lodash 라이브러리의 _.throttle 함수 : 일정 시간마다 함수가 한 번 호출되도록 함
+```javascript
+_.throttle(실행할 함수, 시간)
+```
+사용 예시) 스크롤 이벤트 발생시마다 함수를 호출할 때 함수가 과다 호출되는 것을 방지
+
+<hr>
+
+gsap 애니메이션 처리 라이브러리
+- to 함수
+```javascript
+gsap.to(요소, 지속시간, 옵션)
+```
+
+<hr>
