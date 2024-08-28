7363b00209397a2fb457a2d7b47201708ddaa221
khanna01 <govmfkdtm@gmail.com>
2024-08-28 21:55:09 +0900

    [Add] Video Section

--- course1/starbucks/js/main.js
+++ course1/starbucks/js/main.js
@@ -101,3 +101,27 @@ promotionToggleBtn.addEventListener('click', () => {
     promotionEl.classList.remove('hide')
   }
 })
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+// 애니메이션 적용 함수
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    // 무한반복하도록
+    repeat: -1,
+    // 재생한 애니메이션을 뒤로 재생
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay),
+  })
+}
+
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', 0.5, 15)
+floatingObject('.floating3', 1.5, 20)
