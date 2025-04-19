5559ece18011a296b58065865f53c43023ba6f36
khanna01 <govmfkdtm@gmail.com>
2025-04-19 20:51:13 +0900

    [disney-plus] detailPage

--- course2/disney-plus/src/pages/DetailPage/index.jsx
+++ course2/disney-plus/src/pages/DetailPage/index.jsx
@@ -1,3 +1,33 @@
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from '@/api/axios.js'
+
 export default function DetailPage() {
-  return <div>Detail</div>
+  const { movieId } = useParams()
+  console.log(movieId)
+  const [movie, setMovie] = useState()
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`/movie/${movieId}`)
+      setMovie(response.data)
+      console.log(response.data)
+    }
+    fetchData()
+  }, [movieId])
+
+  if (!movie) return null
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        src={
+          movie.backdrop_path
+            ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+            : `https://image.tmdb.org/t/p/original${movie.poster_path}`
+        }
+        alt="modal__poster-img"
+      />
+    </section>
+  )
 }
